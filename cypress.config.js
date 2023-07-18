db0c668349a2de10330583acfe4b1f207e35457a
Amanda Frateschi <amandafrateschi72@gmail.com>
2023-07-18 16:27:53 -0300

    usando massa de dados

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,10 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   projectId: '3rwvpc',
   e2e: {
+    baseUrl: "http://lojaebac.ebaconline.art.br/",
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
   },
 });
+
